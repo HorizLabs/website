de29bcaacb7deca3272a80390153d5122a027cb4
Lambda <61808223+L-mbda@users.noreply.github.com>
2024-09-28 20:30:37 -0700

    abc

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,4 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  site: 'https://l-mbda.github.io',  // your lowercase username goes here
-  base: '/test/', // your repository name goes here, enclosed in '/'
-  output: 'static',
 });
