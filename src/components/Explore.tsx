032b0bfb69bd5ba9035084049eefc36a7a8dc041
rikhil.tanugula@gmail.com <rikhil.tanugula@gmail.com>
2024-10-03 19:16:47 -0700

    testing

--- src/components/Explore.tsx
+++ src/components/Explore.tsx
@@ -9,7 +9,6 @@ export function Explore() {
         <div className="explore-sys font-[Outfit] bg-[#e9e9e9] dark:bg-[#2b2b2b] text-black dark:text-white p-4 flex flex-col gap-3">
             <h1 className='text-[24px] font-semibold'>Explore our divisions</h1>
             <div className='explore-row'>
-                {/* bg-[#cdcdcd] dark:bg-[#303030] */}
                 <div className='card max-w-[50vw] p-12 max-h-32 min-w-[30vw] bg-white'>
                     <h1 className='text-[35px] ml-4 w-[50%] flex font-[800] text-black'>Software Development</h1>
                 </div>
